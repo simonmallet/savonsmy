9d56a6d7657a5063b8ecb3fc98946cf452a8f489
Simon Mallet <simon@lokkoweb.com>
2021-04-17 18:01:10 -0400

    Updated seeder to create partner & admin users

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -23,15 +23,27 @@ class RolesAndPermissionsSeeder extends Seeder
         $permission = Permission::findOrCreate(User::PERMISSION_FILL_PURCHASE_ORDER);
 
         // create roles and assign created permissions
-        Role::findOrCreate(User::ROLE_SUPER_ADMIN);
+        $adminRole = Role::findOrCreate(User::ROLE_SUPER_ADMIN);
 
         $partnerRole = Role::findOrCreate(User::ROLE_PARTNER)
             ->givePermissionTo([User::PERMISSION_FILL_PURCHASE_ORDER]);
 
-        $user = \App\Models\User::factory()->create([
-            'name' => 'Example User',
-            'email' => 'test@example.com',
-        ]);
-        $user->assignRole($partnerRole);
+        $partnerUser = User::where('email', 'partner@mysite.com')->first();
+        if (!$partnerUser) {
+            $user = \App\Models\User::factory()->create([
+                'name' => 'Partner User',
+                'email' => 'partner@mysite.com',
+            ]);
+            $user->assignRole($partnerRole);
+        }
+
+        $adminUser = User::where('email', 'admin@mysite.com')->first();
+        if (!$adminUser) {
+            $user = \App\Models\User::factory()->create([
+                'name' => 'Admin User',
+                'email' => 'admin@mysite.com',
+            ]);
+            $user->assignRole($adminRole);
+        }
     }
 }
